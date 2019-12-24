bc2216de7eabcde69b7c5abce34d349eebc3a218
biplabdari <58460624+biplabdari@users.noreply.github.com>
2019-12-24 15:30:20 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     getHomePage: (req, res) => {
-        let query = "SELECT * FROM `players` ORDER BY id ASC"; // query database to get all the players
+        let query = "select XXSKU.ITEM_NUMBER SKU, XXSKU.DESCRIPTION,XXPR.LIST_PRICE from XXIBM_PRODUCT_SKU XXSKU,XXIBM_PRODUCT_PRICING XXPR,XXIBM_PRODUCT_STYLE XXPS,XXIBM_PRODUCT_CATALOGUE XXPC where XXSKU.ITEM_NUMBER = XXPR.ITEM_NUMBER and XXSKU.STYLE_ITEM = XXPS.ITEM_NUMBER AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY"; // query database to get all the players
 
         // execute query
         db.query(query, (err, result) => {
